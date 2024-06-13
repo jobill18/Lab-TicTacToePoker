defe233b51e8fc5d2ca45b103ee63c7c0c1e1fc8
Joseph Billings <josephbillings.gb@gmail.com>
2024-06-13 14:27:15 -0600

    added win condition and alert for winner and tie

--- Part1/index.js
+++ Part1/index.js
@@ -16,7 +16,46 @@ function play(evt) {
   }
   const playerSpan = document.querySelector("#current-player");
   playerSpan.innerText = player;
-  console.log(playerSpan.innerText);
+
+  const winner = checkWinner();
+  if (winner) {
+    alert(`${winner} is the winner!`);
+  } else if (boardFull()) {
+    alert("Game is a tie!");
+  }
+}
+
+function checkWinner() {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const line of lines) {
+    const [a, b, c] = line;
+    const squareAText = squares[a].innerText;
+    const squareBText = squares[b].innerText;
+    const squareCText = squares[c].innerText;
+
+    if (squareAText !== "" && (squareAText === squareBText) === squareCText) {
+      return squareAText;
+    }
+  }
+  return undefined;
+}
+
+function boardFull() {
+  for (const square of squares) {
+    if (square.innerText === "") {
+      return false;
+    }
+  }
+  return true;
 }
 
 //Event listeners
