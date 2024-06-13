cec43de4a2a2ee153770385bf60cac7226fe3bfa
Joseph Billings <josephbillings.gb@gmail.com>
2024-06-13 14:39:25 -0600

    part 2 added input fields in html and linked to variable in js

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+console.log("yo");
+
+const grabID = document.querySelector("#grabID");
+const applyStyle = document.querySelector("#applyStyle");
+
+console.log(grabID);
+console.log(applyStyle);
